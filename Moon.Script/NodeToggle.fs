29a27d6d57d2f5b0423cd96421c9f843f63394b8
dasasdhba <dasasdhba@outlook.com>
2026-09-03 22:42:16 +0800

    NodeToggle: sync flag when ready

--- Moon.Script/NodeToggle.fs
+++ Moon.Script/NodeToggle.fs
@@ -5,17 +5,19 @@ open Moon.Component
 open Moon.Library
 
 [<FScript(typeof<NodeToggle>)>]
-type private NodeToggleScript(node : NodeToggle) =
-    
+type private NodeToggleScript(node: NodeToggle) =
+
     let toggle =
         SmoothToggle(node, node.EditorFlag, node.EditorTime, node.EditorPhysics)
-    
+
     do
-        toggle.Value <- node.EditorValue
-        toggle.Paused <- node.EditorPaused
+        toggle.Bind node.OnValueUpdated
         toggle.FullyOn.Add node.EmitSignalFullyOn
         toggle.FullyOff.Add node.EmitSignalFullyOff
-        toggle.Bind node.OnValueUpdated
 
         node.Toggle <- toggle
 
+        node.add_Ready (fun _ ->
+            toggle.Value <- node.EditorValue
+            toggle.Paused <- node.EditorPaused
+        )
